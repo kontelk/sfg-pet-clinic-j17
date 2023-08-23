d3708e4ec87c293f95c88ad9738a05f65f8272b2
kontelk <ktelkis@gmail.com>
2023-08-23 11:30:38 +0300

    Added PetTypeService, closes #29

---
+++ pet-clinic-j17-data/src/main/java/com/kt/sfgpetclinicj17/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package com.kt.sfgpetclinicj17.services;
+
+import com.kt.sfgpetclinicj17.model.PetType;
+
+/**
+ * Created by kontelk on 8/23/23.
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
